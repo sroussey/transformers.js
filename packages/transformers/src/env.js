3458fa5e2fb9406d29fa9a4d77881dd62264dc5c
Joshua Lochner <admin@xenova.com>
2026-03-29 23:40:44 -0400

    [version] Update to 4.0.0-next.11

--- packages/transformers/src/env.js
+++ packages/transformers/src/env.js
@@ -26,7 +26,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
-const VERSION = '4.0.0-next.10';
+const VERSION = '4.0.0-next.11';
 
 const HAS_SELF = typeof self !== 'undefined';
 
